257f54d9a378c7f386e303c3d40b0c6409b8b09f
ins1der23 <m.zadorin@uopt.ru>
2023-04-22 22:26:30 +0500

    SumWIthoutDobles added

---
+++ SumWoutDuplicates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int[] toSum = new int[] { 1, 10, 7, 8, 9, 7, 6, 7, 2 };
+int SumNoDuplicates(int[] arr)
+{
+    Array.Sort(arr);
+    HashSet<int> duplicates = new HashSet<int>();
+    for (int i = 0; i < arr.Length - 1; i++)
+        if (arr[i] == arr[i + 1]) duplicates.Add(arr[i]);
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+        if (!duplicates.Contains(arr[i])) sum += arr[i];
+    return sum;
+}
+Console.WriteLine(SumNoDuplicates(toSum));
+
