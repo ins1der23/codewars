f1050b774afc16e547b3aa5c2ac207755d6684eb
ins1der23 <m.zadorin@uopt.ru>
2023-05-03 15:28:00 +0500

    SecondOccur done

---
+++ SecondnOccur/Program.cs
@@ -0,0 +1,17 @@
+﻿int SecondSymbol(string str, char symbol)
+{
+    int count = 0;
+    int res = -1;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i] == symbol) count++;
+        if (count == 2) 
+        {
+            res = i;
+            break;
+        }
+    }
+    return res;
+}
+
+Console.WriteLine(SecondSymbol("Hello world", 'v')); 
