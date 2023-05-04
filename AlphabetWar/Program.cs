4e1b2cd170242564e8ed8063f57339bffb85b6c5
ins1der23 <m.zadorin@uopt.ru>
2023-05-04 14:30:54 +0500

    Dontwater done

--- AlphabetWar/Program.cs
+++ AlphabetWar/Program.cs
@@ -7,7 +7,7 @@ string AlphabetWar(string fight)
     {'w', 4},
     {'p', 3},
     {'b', 2},
-    {'s',1},
+    {'s', 1},
     {'m',-4},
     {'q',-3},
     {'d',-2},
---
+++ DontWater/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+string MatrixToString(char[,] anyMatrix)
+{
+    string output = String.Empty;
+    for (int rows = 0; rows < anyMatrix.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < anyMatrix.GetLength(1); columns++)
+            output += anyMatrix[rows, columns] + " ";
+        output += Environment.NewLine;
+    }
+    return output;
+}
+
+char[,] SeparateLiquids(char[,] glass)
+{
+    var density = new Dictionary<char, int>()
+ {
+    {'H', 4},
+    {'W', 3},
+    {'A', 2},
+    {'O', 1},
+ };
+
+    char GetKeyFromValue(int valueVar)
+    {
+        foreach (char keyVar in density.Keys)
+            if (density[keyVar] == valueVar) return keyVar;
+        return ' ';
+    }
+
+    int height = glass.GetLength(0);
+    int width = glass.GetLength(1);
+    int size = height * width;
+
+    int[] densityLine = new int[size];
+
+    for (int i = 0; i < height; i++)
+        for (int j = 0; j < width; j++)
+            densityLine[j + i * width] = density[glass[i, j]];
+
+    Array.Sort(densityLine);
+
+    for (int i = 0; i < height; i++)
+        for (int j = 0; j < width; j++)
+            glass[i, j] = GetKeyFromValue(densityLine[j + i * width]);
+
+    return glass;
+}
+
+
+char[,] SeparateLiquidsLinq(char[,] glass)
+{
+
+    var liquidsLine = glass.Cast<char>()
+                  .OrderBy(x => "OAWH".IndexOf(x))
+                  .ToArray();
+
+    int height = glass.GetLength(0);
+    int width = glass.GetLength(1);
+
+    for (int i = 0; i < height; i++)
+        for (int j = 0; j < width; j++)
+            glass[i, j] = liquidsLine[j + i * width];
+
+    return glass;
+}
+
+char[,] actual = new char[,] { { 'A','A','O','H' },
+                                { 'A','H','W','O' },
+                                { 'W','W','A','W' },
+                                { 'H','H','O','O' } };
+
+Console.WriteLine(MatrixToString(SeparateLiquids(actual)));
+
+Console.WriteLine(MatrixToString(SeparateLiquidsLinq(actual)));
