399ea8049e05072cfbf7995358467b431b814be9
ins1der23 <m.zadorin@uopt.ru>
2023-05-05 11:59:14 +0500

    PeopleInTheBus done

---
+++ PeopleInTheus/Program.cs
@@ -0,0 +1,26 @@
+﻿int Number(List<int[]> peopleListInOut)
+{
+    int count = 0;
+    foreach (var stop in peopleListInOut)
+        count += stop[0] - stop[1];
+    return count;
+
+}
+
+int NumberLinq(List<int[]> peopleListInOut)
+  {
+    return peopleListInOut.Sum(Item => Item[0] - Item[1]);
+  }
+
+var passengers = new List<int[]>()
+
+ {
+    new []{10,0},
+    new []{3,5},
+    new []{5,8},
+    new []{9,5},
+    new []{4,5}
+};
+
+
+Console.WriteLine(Number(passengers));
---
+++ VowelCount/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+int GetVowelCount(string str)
+{
+    int vowelCount = 0;
+    string vowels = "aeiou";
+    foreach (var letter in str)
+        if (vowels.Contains(letter)) vowelCount++;
+    return vowelCount;
+}
+
+Console.WriteLine(GetVowelCount("imgoingforgoogle"));
+
+
+int GetVowelCountLinq(string str)
+{
+    return str.Count(i => "aeiou".Contains(i));
+}
