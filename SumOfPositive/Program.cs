a6217468e8675f80fbc4384bb04c65e230038e82
ins1der23 <m.zadorin@uopt.ru>
2023-05-05 12:18:30 +0500

    SumOfPositive done

---
+++ SumOfPositive/Program.cs
@@ -0,0 +1,10 @@
+﻿int PositiveSum(int[] arr)
+{
+    return arr.Where(item => item > 0)
+              .Sum();
+}
+
+
+var numbers = new int[] { -1, -2, -3, -4, -5 };
+
+Console.WriteLine(PositiveSum(numbers));
